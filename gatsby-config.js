bce167a8b2d64e10f0ed566866dce1ad327084c9
camfeghali <camfeg@gmail.com>
2022-10-04 21:59:27 +0200

    integrated formspree

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,14 @@
 module.exports = {
+    siteMetadata: {
+        description: "Personal page of Camille Feghali",
+        locale: "en",
+        title: "Camille Feghali",
+        formspreeEndpoint: "https://formspree.io/f/xqkjbgnb",
+    },
     plugins: [
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
+
         {
             resolve: "@wkocjan/gatsby-theme-intro",
             options: {
